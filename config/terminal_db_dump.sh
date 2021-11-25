4bcf0ceda478aed932e2b41cd0f56c24181b7dad
Robert Wilson <rwilson@ebi.ac.uk>
2021-11-25 14:06:48 +0000

    Revised the dump command.

--- config/terminal_db_dump.sh
+++ config/terminal_db_dump.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-pg_dump --clean --if-exists --no-owner --no-comments --encoding=UTF8 -d "$POSTGRES_DB" -U "$POSTGRES_USER" > /usr/local/data/database.sql
+pg_dump --clean --if-exists --no-comments --encoding=UTF8 -d "$POSTGRES_DB" -U "$POSTGRES_USER" > /usr/local/data/database.sql
 chmod 755 /usr/local/data/database.sql
